9161dc776cee2dc9bd4c745f7c0a23aa7cabb58f
rCarto <timohee.giraud@cnrs.fr>
2020-10-16 12:33:04 +0200

    simplify check, only points

--- R/create_matrix.R
+++ R/create_matrix.R
@@ -2,18 +2,18 @@
 #' @name create_matrix
 #' @description This function creates a distance matrix between two
 #' spatial objects.
-#' @param x an sf object, rows of the distance matrix, row names are used as 
+#' @param x an sf object of points, rows of the distance matrix, row names are used as 
 #' row names of the matrix.
-#' @param y an sf object, columns of the distance matrix, row names are used
+#' @param y an sf objectof points, columns of the distance matrix, row names are used
 #' as column names of the matrix.
-#' @param bypassctrl if TRUE, bypass the distance matrix size control 
+#' @param checksize if FALSE, bypass the distance matrix size control 
 #' (see Details).
 #' @param longlat if FALSE, the Euclidean distance is used, if TRUE Great Circle
 #' (WGS84 ellipsoid) distance is used.
 #' @details The function returns a full matrix of distances in meters.
 #' If the matrix to compute is too large (more than 100,000,000 cells, more than
 #' 10,000,000 origins or more than 10,000,000 destinations)
-#' the function sends a confirmation message to warn users about the amount of
+#' the function may sends a message to warn users about the amount of
 #' RAM mobilized.
 #' @return A distance matrix, row names are \code{x} row names, column
 #' names are \code{y} row names.
@@ -25,52 +25,18 @@
 #' st_distance st_transform st_is
 #' @importFrom methods is
 #' @export
-create_matrix <- function(x, y, bypassctrl = FALSE, longlat = TRUE) {
-  if (bypassctrl == FALSE) {
+create_matrix <- function(x, y, checksize = TRUE, longlat = TRUE) {
+  if (checksize) {
     nk <- nrow(x)
     nu <- nrow(y)
     if (nk * nu > 100000000 | nu > 10000000 | nk > 10000000) {
-      if (interactive()) {
-        cat(
-          "Do you really want to this distance matrix (from", nk,
-          "known points to", nu, "estimated values) ? \n 
-            (It seems to be a heavy computation.) [y/n]"
-        )
-        z <- readLines(con = stdin(), n = 1)
-        while (!z %in% c("n", "y")) {
-          cat("Enter y or n")
-          z <- readLines(con = stdin(), n = 1)
-        }
-        if (z == "y") {
-          cat("Ok, YOLO!")
-        } else {
-          stop("Computation aborted. Matrix would probably be too large.",
-            call. = FALSE
-          )
-        }
-      } else {
-        stop("Computation aborted. Matrix would probably be too large.",
-          call. = FALSE
-        )
-      }
-    }
+      stop(paste0("Computation aborted. The distance matrix would probably ",
+      "be too large. Use checksize = FALSE to bypass this control."),
+           call. = FALSE
+      ) 
+    }  
   }
-  # polygon mngmnt
-  if (!is(st_geometry(x), "sfc_POINT")) {
-    st_geometry(x) <- st_centroid(st_geometry(x),
-      of_largest_polygon = all(
-        st_is(x, "MULTIPOLYGON")
-      )
-    )
-  }
-  if (!is(st_geometry(y), "sfc_POINT")) {
-    st_geometry(y) <- st_centroid(st_geometry(y),
-      of_largest_polygon = all(
-        st_is(y, "MULTIPOLYGON")
-      )
-    )
-  }
-
+  
   if (!st_is_longlat(x)) {
     if (longlat) {
       x <- st_transform(x, 4326)
